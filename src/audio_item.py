a12e632f0bc22e2a505f2e717d8a0cbe7fa1fc54
Tobias Arrskog <topfs2@xbmc.org>
2012-07-30 13:27:39 +0200

    Fixed audio_item

--- src/audio_item.py
+++ src/audio_item.py
@@ -5,6 +5,7 @@ from heimdall import supplies, demands
 from heimdall.predicates import *
 
 import mutagen
+from urlparse import urlparse
 
 class ExtractTags(tasks.SubjectTask):
 	demand = [
@@ -19,8 +20,9 @@ class ExtractTags(tasks.SubjectTask):
 	]
 
 	def run(self):
-		if "file://" == self.subject.uri[7:]:
-			f = mutagen.File(self.subject.uri[7:], easy=True)
+		uri = urlparse(self.subject.uri)
+		if uri.scheme == "file":
+			f = mutagen.File(uri.path, easy=True)
 
 			for album in f.get("album", []):
 				self.subject.emit(upnp.album, album)
@@ -28,8 +30,8 @@ class ExtractTags(tasks.SubjectTask):
 			for artist in f.get("artist", []):
 				self.subject.emit(upnp.artist, artist)
 
-			title = f.get("title", None)
-			if title and len(title) > 0:
+			title = f.get("title", [])
+			if len(title) > 0:
 				self.subject.replace(dc.title, title[0])
 
 			self.subject.Class = "item.audio.musicTrack"
