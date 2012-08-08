83eace2d3f96b56a4f4441bf16d612d7888489ae
Tobias Arrskog <topfs2@xbmc.org>
2012-08-08 12:00:00 +0200

    Guess movie or video based on duration

--- src/video_item.py
+++ src/video_item.py
@@ -4,16 +4,23 @@ from heimdall import resources
 from heimdall import supplies, demands
 from heimdall.predicates import *
 
-class ChangeVideoToMovie(tasks.SubjectTask):
+class GuessVideoOrEpisode(tasks.SubjectTask):
 	demand = [
-		demands.requiredClass("item.video")
+		demands.requiredClass("item.video"),
+		demands.required("video_stream")
 	]
 
 	supply = [
 		supplies.replace(rdf.Class, "item.video.Movie"),
+		supplies.replace(rdf.Class, "item.video.Episode"),
 	]
 
 	def run(self):
-		self.subject.Class = "item.video.Movie"
+		duration = self.subject["video_stream"]["duration"]
 
-module = [ ChangeVideoToMovie ]
+		if duration > 3600: # if longer than an hour, just guess movie
+			self.subject.Class = "item.video.Movie"
+		else:
+			self.subject.Class = "item.video.Episode"
+
+module = [ GuessVideoOrEpisode ]
