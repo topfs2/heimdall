e29904667d54ac968a23c9efff0431a6d10c623c
Tobias Arrskog <topfs2@xbmc.org>
2012-08-20 16:45:07 +0200

    Added docstring for taskqueue

--- src/heimdall/taskqueues.py
+++ src/heimdall/taskqueues.py
@@ -6,9 +6,17 @@ import threading
 import types
 
 class NotFilled(object):
+    """
+    A placeholder for a tasks requirement which is yet to be filled.
+    Internal use only.
+    """
     pass
 
 class TaskData(object):
+    """
+    Structure for necessary data to manage a task.
+    Internal use only.
+    """
     def __init__(self, task, callback):
         self.task = task
         self.callback = callback
@@ -17,6 +25,11 @@ class TaskData(object):
         self.runnable = None
 
 class TaskQueue(object):
+    """
+    A queue which will manage tasks, will schedule out them on the threadpool.
+    The queue will also manage any requirements of given tasks.
+    """
+
     def __init__(self, threadPool):
         self.threadPool = threadPool
 
@@ -26,6 +39,10 @@ class TaskQueue(object):
         self.requirementOwnerMap = dict()
 
     def addTask(self, task, callback):
+        """
+        Method to add a task to the queue. When it has run the result or error
+        will be sent to the callback method.
+        """
         with self.condition:
             self.taskDataMap[task] = TaskData(task, callback)
 
@@ -33,6 +50,10 @@ class TaskQueue(object):
             self._addRunnable(task, rr.runnable, rr.requirements)
 
     def _addRunnable(self, owner, runnable, requirements):
+        """
+        Method which schedules a runnable and its requirement
+        Internal use only.
+        """
         with self.condition:
             self.runnableOwnerMap[runnable] = owner
             taskData = self.taskDataMap[owner]
@@ -53,6 +74,12 @@ class TaskQueue(object):
                 self.threadPool.append(runnable, self.onRunnableDone, 0)
 
     def onRunnableDone(self, runnable, error, result):
+        """
+        Will be called when a runnable is done, will finish the task if it has
+        a result or nothing. If the runnables result is a deferedrun object its
+        requirements and runnable will be scheduled out.
+        Internal use only.
+        """
         with self.condition:
             owner = self.runnableOwnerMap[runnable]
             taskData = self.taskDataMap[owner]
@@ -67,6 +94,10 @@ class TaskQueue(object):
             del self.runnableOwnerMap[runnable]
 
     def onTaskDone(self, task, error, result):
+        """
+        Will be called when a task is done.
+        Internal use only.
+        """
         with self.condition:
             taskData = self.taskDataMap[task]
             taskData.callback(task, error, result)
@@ -74,6 +105,12 @@ class TaskQueue(object):
             del self.taskDataMap[task]
 
     def onRequirementDone(self, r, error, result):
+        """
+        Will be called when finishing task is a requirement to another task.
+        If this task was the last needed requirement the requireing task will be 
+        scheduled.
+        Internal use only.
+        """
         with self.condition:
             owner = self.requirementOwnerMap[r]
             taskData = self.taskDataMap[owner]
