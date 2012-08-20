017a42526a31ddf2b2da2f79be380f451ccb248e
Tobias Arrskog <topfs2@xbmc.org>
2012-08-20 16:59:43 +0200

    Added docstrings for demands

--- src/heimdall/demands.py
+++ src/heimdall/demands.py
@@ -5,10 +5,18 @@ from utils import Enum
 match = Enum([ "NEVER", "NO", "YES" ])
 
 class demand(object):
+    """
+    Base class for what a task may demand
+    """
+
 	def matches(self, subject):
 		raise NotImplementedError("Need must implement matches")
 
 class predicateObjectDemand(demand):
+    """
+    Base class for demands which matches against predicates and possibly objects
+    """
+
 	def __init__(self, predicate, object = None):
 		self.predicate = predicate
 		self.object = object# if object != None else ""
@@ -34,21 +42,37 @@ class predicateObjectDemand(demand):
 	def __repr__(self):
 		return self.__class__.__name__ + " " + self.predicate + ": " + str(self.object)
 
-# Optional predicate, object keypair. The task will be put on hold if any task may provide the pair but will run if it is not provided
 class optional(predicateObjectDemand):
+    """
+    Optional predicate, object keypair. The task will be put on hold if any task
+    may provide the pair but will run if it is not provided.
+    NOT IMPLEMENTED YET.
+    """
 	pass
 
-# A hard dependency, the task will never run if its not provided. The task will wait until on any task which might provide or replace the key-value-pair
 class required(predicateObjectDemand):
+    """
+    A hard dependency, the task will never run if its not provided. The task will
+    wait until on any task which might provide or replace the key-value-pair
+    """
 	pass
 
-# A key-value-pair which may NOT exist on subject for this task to run, the task will be scheduled after any task which might provide or replace the key-value-pair and will
-# be removed from queue if one emits the key-value-pair.
 class none(predicateObjectDemand):
+    """
+    A key-value-pair which may NOT exist on subject for this task to run, the
+    task will be scheduled after any task which might provide or replace
+    the key-value-pair and will be removed from queue if one emits the
+    key-value-pair.
+    """
+
 	def matches(self, subject):
 		return not super(none, self).matches(subject)
 
 class requiredClass(demand):
+    """
+    A demand on the class of a subject.
+    """
+
 	def __init__(self, Class, allowExtended = False):
 		self.Class = Class
 		self.allowExtended = allowExtended
