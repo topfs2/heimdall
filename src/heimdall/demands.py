a4dc84e3da90bde0fe202aac7987fd79d7ca769b
Tobias Arrskog <topfs2@xbmc.org>
2012-08-30 16:31:59 +0200

    Fixed intendation in demands

--- src/heimdall/demands.py
+++ src/heimdall/demands.py
@@ -9,38 +9,38 @@ class demand(object):
     Base class for what a task may demand
     """
 
-	def matches(self, subject):
-		raise NotImplementedError("Need must implement matches")
+    def matches(self, subject):
+        raise NotImplementedError("Need must implement matches")
 
 class predicateObjectDemand(demand):
     """
     Base class for demands which matches against predicates and possibly objects
     """
 
-	def __init__(self, predicate, object = None):
-		self.predicate = predicate
-		self.object = object# if object != None else ""
+    def __init__(self, predicate, object = None):
+        self.predicate = predicate
+        self.object = object# if object != None else ""
 
 # TODO This should be in task rather
-#		if isinstance(self.predicate, types.ListType) or isinstance(self.predicate, types.TupleType):
-#			self.predicate = self.predicate[0]
-#			self.object = self.predicate[1]
+#        if isinstance(self.predicate, types.ListType) or isinstance(self.predicate, types.TupleType):
+#            self.predicate = self.predicate[0]
+#            self.object = self.predicate[1]
 
-		if not isinstance(self.predicate, types.StringTypes):
-			raise ValueError("Predicate must be string type")
+        if not isinstance(self.predicate, types.StringTypes):
+            raise ValueError("Predicate must be string type")
 
-		if not (isinstance(self.object, types.StringTypes) or self.object == None):
-			raise ValueError("Object must be string type or None")
+        if not (isinstance(self.object, types.StringTypes) or self.object == None):
+            raise ValueError("Object must be string type or None")
 
-	def matches(self, subject):
-		obj = subject[self.predicate]
-		if obj and self.object:
-			return re.search(self.object, obj) != None
-		else:
-			return obj != None
+    def matches(self, subject):
+        obj = subject[self.predicate]
+        if obj and self.object:
+            return re.search(self.object, obj) != None
+        else:
+            return obj != None
 
-	def __repr__(self):
-		return self.__class__.__name__ + " " + self.predicate + ": " + str(self.object)
+    def __repr__(self):
+        return self.__class__.__name__ + " " + self.predicate + ": " + str(self.object)
 
 class optional(predicateObjectDemand):
     """
@@ -48,14 +48,14 @@ class optional(predicateObjectDemand):
     may provide the pair but will run if it is not provided.
     NOT IMPLEMENTED YET.
     """
-	pass
+    pass
 
 class required(predicateObjectDemand):
     """
     A hard dependency, the task will never run if its not provided. The task will
     wait until on any task which might provide or replace the key-value-pair
     """
-	pass
+    pass
 
 class none(predicateObjectDemand):
     """
@@ -65,24 +65,24 @@ class none(predicateObjectDemand):
     key-value-pair.
     """
 
-	def matches(self, subject):
-		return not super(none, self).matches(subject)
+    def matches(self, subject):
+        return not super(none, self).matches(subject)
 
 class requiredClass(demand):
     """
     A demand on the class of a subject.
     """
 
-	def __init__(self, Class, allowExtended = False):
-		self.Class = Class
-		self.allowExtended = allowExtended
+    def __init__(self, Class, allowExtended = False):
+        self.Class = Class
+        self.allowExtended = allowExtended
 
-	def matches(self, subject):
-		if self.Class == subject.Class: # Input and sought class are same
-			return True
-		elif re.match(subject.Class, self.Class): # Input class may extend towards sought class, but it has not reached there yet.
-			return False
-		elif re.match(self.Class, subject.Class): # Input class is extended version of sought class
-			return True if self.allowExtended else match.NEVER
-		else: # Input class will never reach sought class, diamond problem
-			return match.NEVER
+    def matches(self, subject):
+        if self.Class == subject.Class: # Input and sought class are same
+            return True
+        elif re.match(subject.Class, self.Class): # Input class may extend towards sought class, but it has not reached there yet.
+            return False
+        elif re.match(self.Class, subject.Class): # Input class is extended version of sought class
+            return True if self.allowExtended else match.NEVER
+        else: # Input class will never reach sought class, diamond problem
+            return match.NEVER
